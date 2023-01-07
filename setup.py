b9de35d16960908681cecd9de90a126e469c0227
JoshyLPHD <108523539+JoshyLPHD@users.noreply.github.com>
2023-01-07 13:07:11 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ system(s + p + "-Syyu " + n)
 system(
     s
     + p
-    + "-S  --needed git neofetch xorg plasma plasma-wayland-session kde-applications nano sl xorg discord dbus libxtst sddm htop base-devel go wget glibc llvm curl make gcc jdk8-openjdk steam zsh "
+    + "-S  --needed git neofetch xorg plasma mkinitcpio plasma-wayland-session kde-applications nano sl xorg discord dbus libxtst sddm htop base-devel go wget glibc llvm curl make gcc jdk8-openjdk steam zsh "
     + n
 )
 system("sudo systemctl enable sddm.service && sudo systemctl enable NetworkManager.service")
