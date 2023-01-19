1a833483fb8d793f5f4c7711634c9527ee3d67cb
JoshyLPHD <108523539+JoshyLPHD@users.noreply.github.com>
2023-01-19 16:45:55 +0100

    Update void.py

--- void.py
+++ void.py
@@ -1,7 +1,16 @@
 from os import system
 
 s= "sudo "
-system(s+"xi flatpak sl cat nano htop ")
-
+xi="xbps-install -S "
+system(s+xi+ "flatpak sl cat nano htop ")
+system("flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo")
+system("flatpak install flathub com.discordapp.Discord")
+system("flatpak install flathub com.valvesoftware.Steam")
+system("flatpak install flathub com.brave.Browser")
+system("flatpak install flathub org.prismlauncher.PrismLauncher")
+system("flatpak install flathub com.visualstudio.code")
+system("flatpak install flathub nz.mega.MEGAsync")
+system("")
+system("")
 
 
