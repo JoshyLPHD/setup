3d544aee2183bcd58ac1b3dee453a43ff69ce228
JoshyLPHD <108523539+JoshyLPHD@users.noreply.github.com>
2023-01-19 19:07:34 +0100

    Update void.py

--- void.py
+++ void.py
@@ -3,7 +3,8 @@ from os import system
 s= "sudo "
 xi="xbps-install -S "
 
-system(s+xi+ "flatpak sl sddm dbus xorg kde5 nano htop -y")
+system(s+xi+ "flatpak sl sddm dbus xorg kde5 zsh wget curl nano zsh-autosuggestions htop -y")
+
 system(s+"flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo")
 system(s+"flatpak install flathub com.discordapp.Discord -y")
 system(s+"flatpak install flathub com.valvesoftware.Steam -y ")
@@ -12,6 +13,7 @@ system(s+"flatpak install flathub org.prismlauncher.PrismLauncher -y ")
 system(s+"flatpak install flathub com.visualstudio.code -y ")
 system(s+"flatpak install flathub nz.mega.MEGAsync -y")
 system("")
-system("")
+system(s+"chsh -s $(which zsh)")
+system('sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended')
 
 
