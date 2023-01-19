6c716283de6f55bedf2f183ae19d2090294ba942
JoshyLPHD <108523539+JoshyLPHD@users.noreply.github.com>
2023-01-19 17:33:57 +0100

    Update void.py

--- void.py
+++ void.py
@@ -5,12 +5,12 @@ xi="xbps-install -S "
 
 system(s+xi+ "flatpak sl dbus xorg kde5 nano htop -y")
 system(s+"flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo")
-system("flatpak install flathub com.discordapp.Discord -y")
-system("flatpak install flathub com.valvesoftware.Steam -y ")
-system("flatpak install flathub com.brave.Browser -y ")
-system("flatpak install flathub org.prismlauncher.PrismLauncher -y ")
-system("flatpak install flathub com.visualstudio.code -y ")
-system("flatpak install flathub nz.mega.MEGAsync -y")
+system(s+"flatpak install flathub com.discordapp.Discord -y")
+system(s+"flatpak install flathub com.valvesoftware.Steam -y ")
+system(s+"flatpak install flathub com.brave.Browser -y ")
+system(s+"flatpak install flathub org.prismlauncher.PrismLauncher -y ")
+system(s+"flatpak install flathub com.visualstudio.code -y ")
+system(s+"flatpak install flathub nz.mega.MEGAsync -y")
 system("")
 system("")
 
