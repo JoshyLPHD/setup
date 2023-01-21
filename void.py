e73d090fd53d7d33d7f194879fb4037446ba8148
JoshyLPHD <108523539+JoshyLPHD@users.noreply.github.com>
2023-01-21 11:10:34 +0100

    Update void.py

--- void.py
+++ void.py
@@ -2,10 +2,10 @@ from os import system
 
 s= "sudo "
 xi="xbps-install -S "
-system(s+"xbps-install -Su -y")
-system(s+xi+ "flatpak plasma-firewall ufw dolphin sl sddm gcc konsole timeshift partitionmanager filelight pulseaudio dbus xorg kde5 zsh wget curl nano htop -y")
+#system(s+"xbps-install -Su -y")
+#system(s+xi+ "flatpak plasma-firewall ufw dolphin sl sddm gcc konsole timeshift partitionmanager filelight pulseaudio dbus xorg kde5 zsh wget curl nano htop -y")
 
-system(s+"flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo")
+#system(s+"flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo")
 system(s+"flatpak install flathub com.discordapp.Discord -y")
 system(s+"flatpak install flathub com.valvesoftware.Steam -y ")
 system(s+"flatpak install flathub com.brave.Browser -y ")
@@ -19,11 +19,11 @@ system(s+"flatpak install flathub com.usebottles.bottles -y")
 system(s+"flatpak install flathub dev.lapce.lapce -y")
 #system(s+"")
 
-system(s+"chsh -s $(which zsh)")
+#system(s+"chsh -s $(which zsh)")
 system('sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended')
 system(s+"git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions")
-system(s+" ln -s /etc/sv/dbus /var/service")
-system(s+" ln -s /etc/sv/sddm /var/service ")
+#system(s+" ln -s /etc/sv/dbus /var/service")
+#system(s+" ln -s /etc/sv/sddm /var/service ")
 system("sudo reboot")
 
 #
