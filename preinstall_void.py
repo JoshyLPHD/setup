ae641265c39136073286d770b6ce9addb4c44de1
JoshyLPHD <108523539+JoshyLPHD@users.noreply.github.com>
2023-01-24 17:47:22 +0100

    Update preinstall_void.py

--- preinstall_void.py
+++ preinstall_void.py
@@ -2,6 +2,7 @@ from os import system
 from time import sleep
 s= "sudo "
 xi="xbps-install -S "
+system("cd ~/ && mkdir Download && mkdir Dokumente")
 system(s+"xbps-install -Su -y")
 system(s+xi+ "flatpak helix sweeper mesa bluez plasma-firewall ufw dolphin sl sddm gcc konsole timeshift partitionmanager filelight pulseaudio dbus xorg kde5 zsh wget curl nano htop -y")
 
