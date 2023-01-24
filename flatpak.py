19ac0ec1ba53e997d911c0d7f3e53bd3046f8695
JoshyLPHD <108523539+JoshyLPHD@users.noreply.github.com>
2023-01-24 17:44:27 +0100

    Create flatpak.py

---
+++ flatpak.py
@@ -0,0 +1,23 @@
+from os import system
+from time import sleep
+
+
+
+s= "sudo "
+xi="xbps-install -S "
+
+
+system(s+"flatpak install flathub com.discordapp.Discord -y")
+system(s+"flatpak install flathub com.valvesoftware.Steam -y ")
+system(s+"flatpak install flathub com.brave.Browser -y ")
+system(s+"flatpak install flathub org.prismlauncher.PrismLauncher -y ")
+system(s+"flatpak install flathub com.visualstudio.code -y ")
+system(s+"flatpak install flathub nz.mega.MEGAsync -y")
+system(s+"flatpak install flathub com.github.tchx84.Flatseal -y")
+system(s+"flatpak install flathub net.davidotek.pupgui2 -y")
+system(s+"flatpak install flathub com.obsproject.Studio -y")
+system(s+"flatpak install flathub com.usebottles.bottles -y")
+system(s+"flatpak install flathub dev.lapce.lapce -y")
+
+sleep(3)
+system("reboot")
