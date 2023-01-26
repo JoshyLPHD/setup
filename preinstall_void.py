049f5bfd23873258bd403dea8d21a9c3abd229db
JoshyLPHD <108523539+JoshyLPHD@users.noreply.github.com>
2023-01-26 09:24:11 +0100

    Update preinstall_void.py

--- preinstall_void.py
+++ preinstall_void.py
@@ -4,7 +4,8 @@ s= "sudo "
 xi="xbps-install -S "
 system("cd ~/ && mkdir Download && mkdir Dokumente")
 system(s+"xbps-install -Su -y")
-system(s+xi+ "flatpak helix sweeper mesa bluez plasma-firewall ufw dolphin sl alacritty sddm gcc konsole timeshift partitionmanager filelight pulseaudio dbus xorg kde5 zsh wget curl nano htop -y")
+##alacritty
+system(s+xi+ "flatpak helix sweeper mesa bluez plasma-firewall ufw dolphin sl  sddm gcc konsole timeshift partitionmanager filelight pulseaudio dbus xorg kde5 zsh wget curl nano htop -y")
 system("cd ~/ && git clone https://bitbucket.org/joshylphd/dotfiles ~/.dotfiles")
 system("rm ~/.zshrc && rm ~/.zshenv")
 system("ln -s ~/.dotfiles/.zshrc ~/.zshrc && ln -s ~/.dotfiles/zshenv ~/.zshenv")
