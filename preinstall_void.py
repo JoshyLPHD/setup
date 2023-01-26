afdfa1aa78b53804f3e1f1df2114bfb933dee49b
JoshyLPHD <108523539+JoshyLPHD@users.noreply.github.com>
2023-01-26 09:10:02 +0100

    Update preinstall_void.py

--- preinstall_void.py
+++ preinstall_void.py
@@ -6,6 +6,7 @@ system("cd ~/ && mkdir Download && mkdir Dokumente")
 system(s+"xbps-install -Su -y")
 system(s+xi+ "flatpak helix sweeper mesa bluez plasma-firewall ufw dolphin sl alacritty sddm gcc konsole timeshift partitionmanager filelight pulseaudio dbus xorg kde5 zsh wget curl nano htop -y")
 system("cd ~/ && git clone https://bitbucket.org/joshylphd/dotfiles ~/.dotfiles")
+system("rm ~/.zshrc && rm ~/.zshenv")
 system("ln -s ~/.dotfiles/.zshrc ~/.zshrc && ln -s ~/.dotfiles/zshenv ~/.zshenv")
 
 system(s+"flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo")
