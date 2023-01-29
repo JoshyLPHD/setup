b8a11754a43bdf11b5cffe6cc0a4068069fd64f5
JoshyLPHD <108523539+JoshyLPHD@users.noreply.github.com>
2023-01-29 08:00:33 +0100

    Update void_install.py

--- void_install.py
+++ void_install.py
@@ -5,7 +5,7 @@ xi="xbps-install -S "
 system("cd ~/ && mkdir Download && mkdir Dokumente")
 system(s+"xbps-install -Su -y")
 ##alacritty
-system(s+xi+ "flatpak helix sweeper mesa bluez plasma-firewall ufw dolphin sl  sddm gcc konsole timeshift partitionmanager filelight pulseaudio dbus xorg kde5 zsh wget curl nano htop -y")
+system(s+xi+ "flatpak helix mesa bluez plasma-firewall ufw dolphin sl  sddm gcc konsole timeshift partitionmanager filelight pulseaudio dbus xorg kde5 zsh wget curl nano htop -y")
 system("cd ~/ && git clone https://bitbucket.org/joshylphd/dotfiles ~/.dotfiles")
 
 system(s+"flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo")
