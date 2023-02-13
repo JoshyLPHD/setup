109f1819f317cc649a2f307cc3d3d176463c86c6
JoshyLPHD <108523539+JoshyLPHD@users.noreply.github.com>
2023-02-13 21:02:26 +0100

    Update endeavour-sway-install.sh

--- endeavour-sway-install.sh
+++ endeavour-sway-install.sh
@@ -4,9 +4,9 @@ s=" sudo "
 nn=" --needed "
 eval "$s $p -Syu $n"
 
-eval '$s + $p -S --needed git clang sway neofetch xorg egl-wayland mesa go mkinitcpio nano sl discord dbus htop base-devel  wget curl make gcc jdk8-openjdk steam zsh "  + n +" && cd /tmp/ && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si $nn $n'
+eval '$s  $p -S --needed git clang sway neofetch xorg egl-wayland mesa go mkinitcpio nano sl discord dbus htop base-devel  wget curl make gcc jdk8-openjdk steam zsh "  + n +" && cd /tmp/ && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si $nn $n'
 
-
+mkdir ~/.dotfiles
 eval "cd ~/ && git clone https://bitbucket.org/joshylphd/dotfiles ~/.dotfiles"
 eval "yay -Syu   $n"
 eval "yay -S $nn wayland wayland-protocols wayland-utils inetutils fusepak blueman bluez virt-manager qemu-tools qemu-system-x86 qemu-img jdk17-openjdk partitionmanager firewalld podman brave-bin dolphin ark konsole jre17-openjdk brave-bin kcalc  filelight plasma-systemmonitor prismlauncher-bin  timeshift-bin visual-studio-code-bin  $n"
