f13669cd7f000ee284fd8e12e5924494cd8c05b4
JoshyLPHD <108523539+JoshyLPHD@users.noreply.github.com>
2023-02-13 20:49:37 +0100

    Update endeavour-sway-install.sh

--- endeavour-sway-install.sh
+++ endeavour-sway-install.sh
@@ -2,9 +2,9 @@ n=" --noconfirm "
 p=" pacman "
 s=" sudo "
 c="$s $p -Syu $n"
-exec $c
+$c
 c='$s + $p + " -S  --needed git clang sway neofetch xorg egl-wayland mesa go mkinitcpio nano sl discord dbus htop base-devel  wget curl make gcc jdk8-openjdk steam zsh "  + n +" && cd /tmp/ && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --needed "+$n'
-exec   $c
+exec   ls
 
 cd ~/ && git clone https://bitbucket.org/joshylphd/dotfiles ~/.dotfiles
 yay -Syyu " + n
