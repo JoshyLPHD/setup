cd6e66b969b07f3d79640dbea2f2d72a7d064495
JoshyLPHD <108523539+JoshyLPHD@users.noreply.github.com>
2023-02-13 20:59:49 +0100

    Update endeavour-sway-install.sh

--- endeavour-sway-install.sh
+++ endeavour-sway-install.sh
@@ -2,28 +2,28 @@ n=" --noconfirm "
 p=" pacman "
 s=" sudo "
 nn=" --needed "
-"$s $p -Syu $n"
+eval "$s $p -Syu $n"
 
-'$s + $p -S --needed git clang sway neofetch xorg egl-wayland mesa go mkinitcpio nano sl discord dbus htop base-devel  wget curl make gcc jdk8-openjdk steam zsh "  + n +" && cd /tmp/ && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si $nn $n'
+eval '$s + $p -S --needed git clang sway neofetch xorg egl-wayland mesa go mkinitcpio nano sl discord dbus htop base-devel  wget curl make gcc jdk8-openjdk steam zsh "  + n +" && cd /tmp/ && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si $nn $n'
 
 
-"cd ~/ && git clone https://bitbucket.org/joshylphd/dotfiles ~/.dotfiles"
-"yay -Syu   $n"
-"yay -S $nn wayland wayland-protocols wayland-utils inetutils fusepak blueman bluez virt-manager qemu-tools qemu-system-x86 qemu-img jdk17-openjdk partitionmanager firewalld podman brave-bin dolphin ark konsole jre17-openjdk brave-bin kcalc  filelight plasma-systemmonitor prismlauncher-bin  timeshift-bin visual-studio-code-bin  $n"
+eval "cd ~/ && git clone https://bitbucket.org/joshylphd/dotfiles ~/.dotfiles"
+eval "yay -Syu   $n"
+eval "yay -S $nn wayland wayland-protocols wayland-utils inetutils fusepak blueman bluez virt-manager qemu-tools qemu-system-x86 qemu-img jdk17-openjdk partitionmanager firewalld podman brave-bin dolphin ark konsole jre17-openjdk brave-bin kcalc  filelight plasma-systemmonitor prismlauncher-bin  timeshift-bin visual-studio-code-bin  $n"
 
-"curl https://sh.rustup.rs -sSf | sh -s -- -y"
-"rustup update"
-"rustup toolchain install nightly"
-"rustup component add rust-src"
-"rustup target install x86_64-pc-windows-gnu"
+eval "curl https://sh.rustup.rs -sSf | sh -s -- -y"
+eval "rustup update"
+eval "rustup toolchain install nightly"
+eval "rustup component add rust-src"
+eval "rustup target install x86_64-pc-windows-gnu"
 
-"$s rm -fr ~/.oh-my-zsh"
-'sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended'
-"$s git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
-"rm ~/.zshrc && rm ~/.zshenv"
-"ln -s ~/.dotfiles/.zshrc ~/.zshrc && ln -s ~/.dotfiles/zshenv ~/.zshenv"
-"$s chsh -s $(which zsh)"
-"$s modprobe nbd max_part=8 "
+eval "$s rm -fr ~/.oh-my-zsh"
+eval 'sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended'
+eval "$s git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+eval "rm ~/.zshrc && rm ~/.zshenv"
+eval "ln -s ~/.dotfiles/.zshrc ~/.zshrc && ln -s ~/.dotfiles/zshenv ~/.zshenv"
+eval "$s chsh -s $(which zsh)"
+eval"$s modprobe nbd max_part=8 "
 echo you should reboot
 #pluginsgit 
 #         extract rust sudo python zsh-autosuggestions
