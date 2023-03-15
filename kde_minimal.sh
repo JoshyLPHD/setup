679437f7562ad3901326657fa83206884aa56f6c
JoshyLPHD <108523539+JoshyLPHD@users.noreply.github.com>
2023-03-15 20:40:49 +0100

    Update kde_minimal.sh

--- kde_minimal.sh
+++ kde_minimal.sh
@@ -15,7 +15,7 @@ mkdir ~/.dotfiles
 eval "cd ~/ && git clone https://bitbucket.org/joshylphd/dotfiles ~/.dotfiles"
 
 eval "yay -Syu   $n"
-eval "yay -S $nn wayland wayland-protocols xorg-xwayland  wayland-utils inetutils fusepak blueman bluez  jdk17-openjdk partitionmanager firewalld dolphin ark konsole jre17-openjdk kcalc plasma-systemmonitor timeshift-bin   $n"
+eval "yay -S $nn wayland wayland-protocols xorg-xwayland  wayland-utils inetutils fusepak blueman bluez   partitionmanager firewalld dolphin ark konsole  kcalc plasma-systemmonitor timeshift-bin   $n"
 
 eval "$s rm -fr ~/.oh-my-zsh"
 eval 'sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended'
