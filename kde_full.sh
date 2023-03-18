fde58d8c7150bb49c3079f31256aa1f80219e861
JoshyLPHD <108523539+JoshyLPHD@users.noreply.github.com>
2023-03-18 12:50:33 +0100

    Update kde_full.sh

--- kde_full.sh
+++ kde_full.sh
@@ -54,6 +54,18 @@ eval "ln -s ~/.dotfiles/.zshrc ~/.zshrc && ln -s ~/.dotfiles/zshenv ~/.zshenv"
 eval "$s chsh -s $(which zsh)"
 eval "echo 'exec zsh' >> ~/.bashrc "
 
+echo '#/bin/sh
+
+eval "curl https://sh.rustup.rs -sSf | sh -s -- -y"
+eval "rustup toolchain install nightly"
+eval "rustup target install x86_64-unknown-linux-musl
+eval "rustup target install x86_64-unknown-none
+eval "rustup target install wasm32-unknown-unknown
+eval "rustup component add rust-src"
+eval "rustup target install x86_64-pc-windows-gnu "' > kekw.tmp
+zsh --exec  $(pwd)/kekw.tmp
+rm kekw.tmp -f
+
 eval "sudo systemctl enable sddm.service && sudo systemctl enable NetworkManager.service"
 echo "you should reboot"
 
