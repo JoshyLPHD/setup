9c4a2bf77aa7a8caed89f7f384a4255c6d0d7e3e
JoshyLPHD <108523539+JoshyLPHD@users.noreply.github.com>
2023-03-18 12:18:30 +0100

    Update kde_full.sh

--- kde_full.sh
+++ kde_full.sh
@@ -8,14 +8,20 @@ s=" sudo "
 nn=" --needed "
 eval "$s $p -Syu $n"
 
-eval '$s  $p -S $nn git clang plasma-wayland-session plasma xorg egl-wayland mesa go sddm mkinitcpio nano dbus htop base-devel wget curl make gcc jdk8-openjdk zsh $n && cd /tmp/ && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si  $n'
+eval '$s  $p -S $nn git clang wget curl make ttf-liberations cmake mesa neofetch  gcc plasma-wayland-session pipewire-alsa wireplumber pipwire-jack xorg   mkinitcpio nano dbus htop base-devel wget curl make gcc jdk8-openjdk zsh $n && cd /tmp/ && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si  $n'
 mkdir ~/.local/bin
 
 mkdir ~/.dotfiles
 eval "cd ~/ && git clone https://bitbucket.org/joshylphd/dotfiles ~/.dotfiles"
 
 eval "yay -Syu   $n"
-eval "yay -S $nn wayland git neofetch xorg plasma ttf-liberations cmake egl-wayland podman gamemode vscodium-bin mesa go mkinitcpio prismlauncher-bin plasma-wayland-session nano sl discord dbus sddm htop base-devel  wget curl make gcc jdk8-openjdk steam zsh wayland-protocols xorg-xwayland  wayland-utils inetutils fusepak blueman bluez  jdk17-openjdk partitionmanager firewalld neofetch dolphin ark konsole jre17-openjdk kcalc plasma-systemmonitor timeshift-bin   $n"
+eval "yay -S $nn wayland 
+ plasma  go  egl-wayland   sddm 
+neofetch  plasma   egl-wayland podman gamemode vscodium-bin   mkinitcpio prismlauncher-bin plasma-wayland-session 
+nano sl discord dbus sddm htop base-devel 
+ jdk8-openjdk steam zsh wayland-protocols xorg-xwayland 
+wayland-utils inetutils fusepak blueman bluez  jdk17-openjdk partitionmanager firewalld 
+dolphin ark konsole jre17-openjdk kcalc plasma-systemmonitor timeshift-bin   $n"
 
 eval "$s rm -fr ~/.oh-my-zsh"
 eval 'sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended'
